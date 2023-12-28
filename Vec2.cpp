9facfa218a982360a79eec39e50702439b07e528
Tom lima <tom@tomlima.me>
2023-12-27 22:24:54 -0300

    Add vector2 class

---
+++ Vec2.cpp
@@ -0,0 +1,64 @@
+#include "Vec2.h" 
+#include <cmath>
+
+// Default constructor
+Vec2::Vec2(): x(0), y(0) {}
+
+// Constructor with initializer list
+Vec2::Vec2(float xin, float yin) : x(xin), y(yin) {};
+
+bool Vec2::operator==(const Vec2& vec) const {
+	return (x == vec.x && y == vec.y); 
+}
+
+bool Vec2::operator==(const float val) const {
+	return (x == val && y == val); 
+}
+
+bool Vec2::operator!=(const Vec2& vec) const {
+	return (x != vec.x && y != vec.y); 
+}
+
+Vec2 Vec2::operator+(const Vec2& vec) const {
+	return Vec2(x + vec.x, y + vec.y); 
+}
+
+Vec2 Vec2::operator-(const Vec2& vec) const {
+	return Vec2(x - vec.x, y - vec.y); 
+}
+
+Vec2 Vec2::operator/(const float val) const {
+	return Vec2(x / val, y / val);
+}
+
+Vec2 Vec2::operator*(const float val) const {
+	return Vec2(x * val, y * val);
+}
+
+void Vec2::operator+=(const Vec2& vec) {
+	x += vec.x;
+	y += vec.y;
+}
+
+void Vec2::operator-=(const Vec2& vec) {
+	x -= vec.x;
+	y -= vec.y;
+}
+
+void Vec2::operator*=(const float val) {
+	x *= val;
+	y *= val;
+}
+
+void Vec2::operator/=(const float val) {
+	x /= val;
+	y /= val;
+}
+
+float Vec2::dist(const Vec2& vec) const {
+	return sqrt(pow((x - vec.x), 2) + pow((y - vec.y), 2));
+}
+
+bool Vec2::operator>(const float val) const {
+	return ((x > val) && (y > val));
+}
